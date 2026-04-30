6393d517bf10d3a1fe4ec4c8ea6f6f1e72f194d9
Thomas Schmiereck <thomas@schmiereck.de>
2026-04-30 08:33:43 +0200

    add gripper camera.

--- launch/all_cameras.launch.py
+++ launch/all_cameras.launch.py
@@ -1,23 +1,23 @@
 """Launch both overview and gripper cameras."""
 
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.substitutions import FindPackageShare
-import os
 
 def generate_launch_description():
-    pkg_share = FindPackageShare('ba_camera_bridge')
+    pkg_share = get_package_share_directory('ba_camera_bridge')
     
     overview_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_share.find('ba_camera_bridge'), 'launch', 'overview_camera.launch.py')
+            os.path.join(pkg_share, 'launch', 'overview_camera.launch.py')
         )
     )
     
     gripper_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_share.find('ba_camera_bridge'), 'launch', 'gripper_camera.launch.py')
+            os.path.join(pkg_share, 'launch', 'gripper_camera.launch.py')
         )
     )
     
